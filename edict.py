1e3761a49747d252f94e0cf9d8ec14db13a641e5
mathslinux <riegamaths@gmail.com>
2011-08-30 23:54:48 +0800

    Version 0.0.2 released

--- edict.py
+++ edict.py
@@ -11,9 +11,16 @@ Version: 0.0.2
 __author__ = 'mathslinux'
 __version__ = '0.0.2'
 
-# Append xgoogle to system path
 import sys, os
 import getopt
+import urllib, urllib2
+import re
+from struct import unpack
+
+try:
+    import simplejson as json
+except:
+    raise ImportError, 'Cant find simplejson, please install it.'
 
 _engine = ('google', 'stardict')
 
@@ -111,6 +118,9 @@ _languages = {
     'yi': 'Yiddish'
     };
 
+class TranslationError(Exception):
+    pass
+
 class GoogleTranslator(object):
     """ Google search engine
     """
@@ -125,12 +135,12 @@ class GoogleTranslator(object):
         """
 
         if t not in _languages:
-            raise 'Translate to language %s is not supported.' % t
-        if not f and f not in _languages:
-            raise 'Translate from language %s is not supported' % (f)
+            raise TranslationError, 'Translate to language %s is not supported.' % t
+        if f and f not in _languages:
+            raise TranslationError, 'Translate from language %s is not supported' % f
 
         buf = {'v': '1.0',
-               'q': 'search_text',
+               'q': '%s' % search_text,
                'langpair': '%s|%s' %(f, t)
                }
         buf = urllib.urlencode(buf)
@@ -139,22 +149,104 @@ class GoogleTranslator(object):
             resp = urllib2.urlopen(translate_url, buf)
             data = json.loads(resp.read())
             if data['responseStatus'] != 200:
-                raise "Something error happen."
+                raise TranslationError, "Response error."
             return data['responseData']['translatedText']
         except:
-            raise "Something error happen."
+            raise TranslationError, "Something error happen."
         
         return None
+
+
+class StardictIdx(object):
+    """ Parse the dictionary of stardict format
+    
+    the format is stored in .idx file, which looks like:
+    |word|offset|length|
+    word: terminated by '\0'.
+    offset: translation's offset in .dict file
+    length: translation's length in .dict file
+    
+    """
+
+    def __init__(self, idx_file):
+        self.idx = self._get_dict_idx(idx_file)
         
+
+    def _get_dict_idx(self, idx_file):
+        """ Parse the .idx file
+        
+        the .idx file will be parse to dict idx:
+        {
+        word1: (offset1, length1),
+        word2: (offset2, length2),
+        ... ...
+        wordn: (offsetn, lengthn)
+        }
+        """
+
+        idx = {}
+        idx_buf = open(idx_file, 'rb').read()
+        pattern = r'(.+?\x00.{8})'
+        for record in re.findall(pattern, idx_buf):
+            c = record.find('\x00')
+            word = record[0:c]
+            offset, length = unpack('!II', record[-8:])
+            idx[word] = (offset, length)
+        return idx
+
+    def __getitem__(self, word):
+        for i in self.idx.keys():
+            if cmp(word.upper(), i.upper()) == 0:
+                return self.idx[i]
+        
+    
 class StardictTranslator(object):
-    """ Use stardict to translate
+    """ Translation from stardict dictionary
+    """
+    
+    def __init__(self, idx_file, dict_file):
+        self.dict_file = dict_file
+        self.idx = StardictIdx(idx_file)
+
+    def _get_translation(self, word):
+
+        try:
+            offset, length = self.idx[word]
+        except TypeError:
+            raise TranslationError
+
+        _dict_file = open(self.dict_file, 'r')
+        _dict_file.seek(offset)
+        buf = _dict_file.read(length)
+        _dict_file.close()
+
+        return buf
+
+    def translate(self, search_text, f='', t='zh'):
+        """ Do the translating
+        """
+
+        if (f and f != 'en') or t != 'zh':
+            raise TranslationError
+
+        return self._get_translation(search_text)
+            
+    
+
+class ICIBATranslator(object):
+    """ Use iciba api to translate
     """
 
     def __init__(self):
+        """
+        """
         pass
 
-    def translate(self, search_text):
-        return 'Not support now'
+    def translate(self):
+        """
+        """
+
+        return None
         
 def usage():
     usage = """Usage: edict [options] text
@@ -170,22 +262,31 @@ def usage():
            to language"""
     print usage
 
+
+_current_path = os.path.split(__file__)[0]
+_idx_file = os.path.join(_current_path, 'stardict-oxford-gb-2.4.2/oxford-gb.idx')
+_dict_file = os.path.join(_current_path, 'stardict-oxford-gb-2.4.2/oxford-gb.dict')
 def do_search(engine, search_text, f, t):
     if engine not in _engine:
         print 'Not support search engine.'
 
     translation = ''
-    if engine == 'google':
-        translation = GoogleTranslator().translate(search_text, f, t)
-    elif engine == 'stardict':
-        translation = StardictTranslator.translate(search_text, f, t)
-    else:
-        return 'No such engine %s' %(self.engine)
 
-    if translation:
-        return translation
-    else:
-        print 'Something error.'
+    try:
+        if engine == 'google':
+            translation = GoogleTranslator().translate(search_text, f, t)
+            translation = translation.encode('utf-8')
+        elif engine == 'stardict':
+            translation = StardictTranslator(_idx_file, _dict_file).translate(search_text, f, t)
+        else:
+            return 'No such engine %s' %(self.engine)
+
+        print search_text
+        print translation
+
+    except TranslationError:
+        print search_text
+        print '!!!translate error.'
                 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
@@ -194,7 +295,7 @@ if __name__ == '__main__':
 
     engine = 'google'
     f = ''
-    t = 'en'
+    t = 'zh'
     
     try:
         opts, args = getopt.getopt(sys.argv[1:], "hve:f:t:", ["help", "version", "engine=", "from=", "to="])
@@ -218,9 +319,8 @@ if __name__ == '__main__':
             t = a
         else:
             assert False, "invalid option %s" %(o)
-            
+
     search_text = ' '.join(args).strip()
-    print search_text
-    print do_search(engine, search_text, f, t)
+    do_search(engine, search_text, f, t)
 
     
